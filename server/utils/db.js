718dc3215a1e6840d8d92065e91c48beadfc9b19
leifriksheim <riksheim.leif@gmail.com>
2020-12-06 22:24:58 +0100

    db defaults

--- server/utils/db.js
+++ server/utils/db.js
@@ -3,3 +3,5 @@ import FileSync from "lowdb/adapters/FileSync.js";
 
 const adapter = new FileSync("db.json");
 export const database = low(adapter);
+
+database.defaults({ lights: [], devices: {}, scenes: [] }).write();
