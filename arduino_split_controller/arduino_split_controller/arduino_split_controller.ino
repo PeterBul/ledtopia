e49bda06b64d04cf38a3ac039f2168904f8b1af8
PeterBul <pc.bulukin@gmail.com>
2023-07-09 23:48:53 +0200

    fix: Add brightness to controller

--- arduino_split_controller/arduino_split_controller/arduino_split_controller.ino
+++ arduino_split_controller/arduino_split_controller/arduino_split_controller.ino
@@ -1,3 +1,9 @@
+/*
+This is a controller that sends data to Ledtopia.
+It is meant to be used on an Arduino or other microcontroller without WiFi.
+It sends data to an ESP8266 through a serial connection.
+*/
+
 #include <SoftwareSerial.h>
 #include <ArduinoJson.h>      // Handle JSON
 
@@ -16,11 +22,13 @@ void loop() {
 int state = 0;
 int cp = 0;
 
+int brightness = 255;
 void sendState()
 {
-  delay(5000);
+    delay(5000);
     DynamicJsonDocument doc(200);
     doc["State"] = state;
+    doc["Brightness"] = brightness;
     state = (state + 1) % 5;
     char data[200];
     serializeJson(doc, data);
